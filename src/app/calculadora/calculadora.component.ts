62729dd73fa35bfde5ff8faa3db87aba9a21cfe7
Cesar-CAAR <cesar.andres5242@gmail.com>
2025-03-17 22:14:38 -0700

    Implemented Input and Output

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from '../formulario/formulario.component';
+import { ResultadoComponent } from '../resultado/resultado.component';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [RouterOutlet, FormsModule],
+  imports: [FormsModule, FormularioComponent, ResultadoComponent],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+  
+  resultadoPadre:number = 0;
 
-
-  numero1: number = 0;
-  numero2: number = 0;
-
-
-  sumar(): number {
-    return this.numero1 + this.numero2;
+  actualizarResultado(resultado: number){
+    this.resultadoPadre = resultado;
   }
+ 
 }
